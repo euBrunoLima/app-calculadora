140ceb94b0574c7b9f255d7512d624e5228d6548
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2024-08-20 15:38:13 -0300

    Melhorias no js

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-function calcular(tipo,valor){
-    if(tipo === 'simbolo'){
+function calcular(tipo, valor) {
+    let visor = document.getElementById('visor');
+    let visorValue = visor.value;
+
+   if(tipo === 'simbolo'){
         if(valor === 'c'){
             document.getElementById('visor').value = ''
-        }
-        else if(valor === 'X'){
-            let caracter = document.getElementById('visor').value
-            caracter = caracter.slice(0, -1)
-
-            document.getElementById('visor').value = caracter
-        }
-        else if(valor === '÷' || valor === '×' || valor === '-' || valor === '+' || valor === ','){
-            document.getElementById('visor').value += valor
-        }
+            }else if(valor === 'X'){
+                visor.value = visorValue.slice(0,-1)
+            }else if(valor === '%'){
+                visor.value = (visorValue/100) * 10
+                visor.value = ''
+                visor.placeholder = (visorValue/100) * 10
+                
+            }else if(valor === '='){
+                if(visorValue === ''){
+                    visor.value = ''
+                    visor.placeholder = '0'
+                    return
+                }
+                try{
+                    let expressao = visorValue.replace(/÷/g,'/').replace(/×/g,'*').replace(/%/g,'/100')
+                    if(carcterConsecutivo(expressao)){
+                        visor.value = ''
+                        visor.placeholder = "Syntax error"
+                    }
+                    else{
+                        visor.value = ''
+                        visor.placeholder = eval(expressao)
+                    }
+                }
+                catch(erro){
+                    visor.value = ''
+                    visor.placeholder = "Syntax error"
+                }
+            }
+        else
+            visor.value += valor
     }
- 
-    else if(tipo === 'numero'){
-    document.getElementById('visor').value += valor
-    }
- 
-
-
+    else if(tipo === 'numero')
+        visor.value += valor
+}
+function carcterConsecutivo(str) {
+    const regex = /([+'/'-*÷])\1+/g;
+    return regex.test(str);
+}
+function limparPlaceholder(visor){
+    visor.value = ''
+    visor.placeholder = "Syntax error"
 }
